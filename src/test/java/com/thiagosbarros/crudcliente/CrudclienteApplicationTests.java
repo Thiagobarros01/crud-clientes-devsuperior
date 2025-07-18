54842096839d19f382c441435ac5641b4d0b450c
Thiago <thiagosbarros_@hotmail.com>
2025-07-17 21:42:19 -0300

    initial commit

---
+++ src/test/java/com/thiagosbarros/crudcliente/CrudclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagosbarros.crudcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
