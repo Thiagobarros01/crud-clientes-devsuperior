c928804b24905bae44148828b910ebbf617a13e3
Thiago <thiagosbarros_@hotmail.com>
2025-08-02 12:44:45 -0300

    initial commit

--- src/main/java/com/thiagosbarros/crudcliente/controllers/ClientController.java
+++ src/main/java/com/thiagosbarros/crudcliente/controllers/ClientController.java
@@ -1,5 +1,11 @@
 package com.thiagosbarros.crudcliente.controllers;
 
+import com.thiagosbarros.crudcliente.dto.ClientDto;
+import com.thiagosbarros.crudcliente.services.ClientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/client")
 public class ClientController {
 
+     private ClientService clientService;
 
+     public ClientController(ClientService clientService) {
+         this.clientService = clientService;
+     }
+
+     @PostMapping
+     public ResponseEntity<ClientDto> createClient(@RequestBody ClientDto clientDto){
+         ClientDto clienteDto = clientService.create(clientDto);
+         return ResponseEntity.status(HttpStatus.CREATED).body(clienteDto);
+     }
 
 }
--- src/main/java/com/thiagosbarros/crudcliente/services/ClientService.java
+++ src/main/java/com/thiagosbarros/crudcliente/services/ClientService.java
@@ -1,8 +1,13 @@
 package com.thiagosbarros.crudcliente.services;
 
 import com.thiagosbarros.crudcliente.dto.ClientDto;
+import com.thiagosbarros.crudcliente.entities.Client;
+import com.thiagosbarros.crudcliente.mapper.ClientMapper;
 import com.thiagosbarros.crudcliente.repository.ClientRepository;
+import jakarta.websocket.ClientEndpoint;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -13,8 +18,33 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
 
-    public ClientDto salvar(ClientDto clientDto) {
+    public ClientDto create(ClientDto clientDto) {
+        Client client = new  Client();
+        client.setId(clientDto.getId());
+        client.setName(clientDto.getName());
+        client.setBirthDate(clientDto.getBirthDate());
+        client.setChildren(clientDto.getChildren());
+        client.setCpf(clientDto.getCpf());
+        client.setIncome(clientDto.getIncome());
+        clientRepository.save(client);
+        return ClientMapper.toDto(client);
 
     }
+
+    public ClientDto update(ClientDto clientDto) {
+        return null;
+    }
+
+    public void delete(ClientDto clientDto) {
+
+    }
+
+    public ClientDto findById(ClientDto clientDto) {
+        return null;
+    }
+    public List<ClientDto> findAll() {
+        return null;
+    }
+
 
 }
