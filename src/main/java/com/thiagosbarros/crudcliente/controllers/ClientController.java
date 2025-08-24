c865a2e97eb25c4326f6b66917ddbd8e189e1ca7
Thiago <thiagosbarros_@hotmail.com>
2025-08-24 12:09:02 -0300

    Finalizando desafio client

--- src/main/java/com/thiagosbarros/crudcliente/controllers/ClientController.java
+++ src/main/java/com/thiagosbarros/crudcliente/controllers/ClientController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/client")
+@RequestMapping("/clients")
 public class ClientController {
 
      private ClientService clientService;
