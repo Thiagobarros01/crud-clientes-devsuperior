297d0c3524d1f4895ee41d9cd2888a7b9e3904a3
Thiago <thiagosbarros_@hotmail.com>
2025-09-08 15:40:30 -0300

    Implementando security with formLogin

---
+++ src/main/java/com/thiagosbarros/crudcliente/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.thiagosbarros.crudcliente.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+
+        var user = User.withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //Configuração de regra de acesso
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) //CSRF PARA API REST É DESABILITADO
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuador/health").permitAll()
+                        // GET pode ser feito por USER or ADMIN
+                        .requestMatchers(HttpMethod.GET, "/clients/**").hasAnyRole("ADMIN", "USER")
+
+                        // POST, DELETE or PUT poder ser feito por ADMIN
+                        .requestMatchers(HttpMethod.POST, "/clients/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/clients/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/clients/**").hasRole("ADMIN")
+                        // qualquer outra requisição precisa estar autenticada
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.loginPage("/login"));
+        return http.build();
+
+    }
+
+}
